f96a67c0916f8b2f09ace6955e93f0f7cce7f359
kayquelopes <kaiquelopes.601@gmail.com>
2026-04-10 08:53:34 -0300

    feat: inicial commit

---
+++ backend/gerenciador-biblioteca/src/test/java/com/biblioteca/gerenciador/GerenciadorBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
